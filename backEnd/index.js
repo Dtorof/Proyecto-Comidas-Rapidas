6cbaf4e1d014a12b2d23742ed6cb174143ec3717
Anyelith Jacobo <anyelithj@misena.edu.co>
2022-10-10 18:23:24 -0500

    modifications in the cart commit

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,12 +1,12 @@
 new Vue({   
   el: '#app',
   data: {
-  quantity:0,
   cartData: [],
   additionals:0,
   totalPayment: "",
   password: "",
   username: "",
+  quantity: 0,
   loguedUser:[],
   registeredUsers:[{name: "Oscar",username:"admin",password:"admin", rol: "administrator"}],
   allAdditionals: [],
@@ -25,7 +25,6 @@ new Vue({
       // console.log('new data');
       console.log(this.allProducts)
       this.setterLocalStorage(this.REGISTERED_USERS_KEY, this.registeredUsers)
-      this.addQuantityToProducts();
   },
   methods: {
     setterLocalStorage(key, data) {
@@ -34,31 +33,28 @@ new Vue({
     getterLocalStorage(key) {
       return JSON.parse(localStorage.getItem(key) || "[]");
     },
-    addQuantityToProducts(){
-      if (this.allProducts?.burgers){
-        this.allProducts.burgers = this.allProducts.burgers.map(prod =>({...prod, qty: 1}))
-      }
-      if (this.allProducts?.hot_dogs){
-        this.allProducts.hot_dogs = this.allProducts.hot_dogs.map(prod => ({...prod, qty: 1}))
-      }
-    },
     addCartBurgers(itemId) {
       const productBuy = this.allProducts.burgers.find((prod)=> prod.id === itemId)
       productBuy.quantity = productBuy.qty;
       productBuy.subTotal = this.thousandSeparator((productBuy.qty * productBuy.price) + this.additionals);
       productBuy.subTotalNumber =  (productBuy.qty * productBuy.price) + this.additionals;
-      this.addProduct(productBuy)
+      // this.addProduct(productBuy)
+      this.cartData.push(productBuy);
+      console.log(productBuy)
     },
     addCartHotDog(itemId) {
       const productBuy = this.allProducts.hot_dogs.find((prod)=> prod.id === itemId)
       productBuy.quantity = productBuy.qty;
       productBuy.subTotal =  this.thousandSeparator((productBuy.qty * productBuy.price) + this.additionals);
       productBuy.subTotalNumber =  (productBuy.qty * productBuy.price) + this.additionals;
-      this.addProduct(productBuy)
+      this.cartData.push(productBuy);
+      // this.addProduct(productBuy)
     },
+
     addProduct(productBuy){
       this.cartData.push(productBuy);
-      console.log(this.cartData)
+      // this.showConfirmation();
+      console.log(this.cartData)       
     },
     thousandSeparator(number = 0, decimalsQuantity = 2) {
       return Number(number).toFixed(decimalsQuantity).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -173,6 +169,7 @@ new Vue({
         "id": this.v4(),
         "name": "Básica",
         "price": 15000,
+        "qty": 0,
         "description": "Hamburguesa con carne de 50gr y queso derretido.",
         "image": "https://res.cloudinary.com/jorge-tarifa/image/upload/v1665118196/carrito-market-mix/Sencilla_c4zql2.jpg",
         "additional": []
@@ -181,6 +178,7 @@ new Vue({
         "id": this.v4(),
         "name": "Bacon",
         "price": 25000,
+        "qty": 0,
         "description": "Hamburguesa con dos carnes de res de 50gr,  125 gr de tocineta, queso cheddar, cebolla, salsa de tomate y mostaza.",
         "image": "https://res.cloudinary.com/jorge-tarifa/image/upload/v1665118200/carrito-market-mix/Bacon_h8vzrt.jpg",
         "additional": []
@@ -188,6 +186,7 @@ new Vue({
         "id": this.v4(),
         "name": "Magna",
         "price": 60000,
+        "qty": 0,
         "description": "Prueba la deliciosa Magna. Cinco carnes de res de 50gr con salsa especial de la casa y queso derretido.",
         "image": "https://res.cloudinary.com/jorge-tarifa/image/upload/v1665118200/carrito-market-mix/Magna_hoka4e.jpg",
         "additional": []
@@ -195,6 +194,7 @@ new Vue({
         "id": this.v4(),
         "name": "Triple Carne",
         "price": 35000,
+        "qty": 0,
         "description": "Hamburguesa con tres carnes de 50gr, dos queso cheddar, cebolla, pepinillos, salsa de tomate y mostaza.",
         "image": "https://st3.depositphotos.com/3957801/12810/i/600/depositphotos_128102518-stock-photo-big-beef-burger.jpg",
         "additional": []
@@ -203,6 +203,7 @@ new Vue({
         "id": this.v4(),
         "name": "Básico",
         "price": 9000,
+        "qty": 0,
         "description": "Hot dog básico con mostaza y pan recién horneado.",
         "image": "https://res.cloudinary.com/jorge-tarifa/image/upload/v1665118573/carrito-market-mix/Hot-sencillo_pzc1oe.jpg",
         "additional": []
@@ -210,6 +211,7 @@ new Vue({
         "id": this.v4(),
         "name": "Texano",
         "price": 20000,
+        "qty": 0,
         "description": "Hot dog con salchicha texana, lechuga, tomate, cebolla, chips de patata y un toque de queso.",
         "image": "https://res.cloudinary.com/jorge-tarifa/image/upload/v1665117307/carrito-market-mix/Texano_uccuwd.jpg",
         "additional": []
@@ -217,6 +219,7 @@ new Vue({
         "id": this.v4(),
         "name": "Viena",
         "price": 15000,
+        "qty": 0,
         "description": "Hot dog con salchicha tipo viena,lechuga, tomate, cebolla y un toque de queso.",
         "image": "https://res.cloudinary.com/jorge-tarifa/image/upload/v1665117299/carrito-market-mix/Viena_i7rbd7.jpg",
         "additional": []
@@ -224,6 +227,7 @@ new Vue({
         "id": this.v4(),
         "name": "Quesudo",
         "price": 17000,
+        "qty": 0,
         "description": "Hot dog con salchicha especial de la casa,lechuga, tomate, cebolla y abundante queso derretido.",
         "image": "https://res.cloudinary.com/jorge-tarifa/image/upload/v1665117323/carrito-market-mix/Cheeser_uwf5us.jpg",
         "additional": []
@@ -235,22 +239,8 @@ new Vue({
   createNewProduct(){
   
   },
-      addCart(itemId) {
-        this.productBuy = this.products.burgers.find((prod)=> {
-          if(prod.id === itemId) {
-            console.log(prod.id === itemId)
-            console.log(prod)
-            return prod
-          }
-        })
-        this.addProduct(this.productBuy)
-        return this.productBuy
-      },
-      addProduct(){
-        this.cartData.push(this.productBuy);
-        return this.cartData;
-      },
       validation(){
+
         let closeModal = document.getElementById('not');
             closeModal.click();
         let closeModal2 = document.getElementById('not1');
